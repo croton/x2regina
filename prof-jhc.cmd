ea86efb285aa6e63bcd28a277447e951a59f4ba4
Cel Pena <celestino.pena@gmail.com>
2024-08-06 13:43:39 -0400

    Update profiles to reflect macro changes

--- prof-jhc.cmd
+++ prof-jhc.cmd
@@ -1,9 +1,10 @@
 @copy XW32.PRO XDEF.PRO 2>nul
-xprof default.prof cjp.prof colors.prof es6.prof html.prof tags.prof css.prof
+xprof default.prof cjp.prof colors.prof es6.prof jdom.prof html.prof tags.prof css.prof
 @pause
 @move XW32.PRO JHC.PRO
 @move XDEF.PRO XW32.PRO
 @attrib -A es6.prof
+@attrib -A jdom.prof
 @attrib -A html.prof
 @attrib -A tags.prof
 @attrib -A css.prof
